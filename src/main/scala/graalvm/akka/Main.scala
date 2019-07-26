9a6ebee8aa6782cc00f70580806f13a93abd37b5
Dean.Zhang <dean.zhang@kongming-inc.com>
2019-07-26 11:29:07 +0800

    tried native image compilation

--- src/main/scala/graalvm/akka/Main.scala
+++ src/main/scala/graalvm/akka/Main.scala
@@ -15,16 +15,16 @@ import scala.concurrent.duration.{Duration, DurationInt}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    //val confPath = if (args.length > 0) args(0) else "./reference.conf"
-    //val confFile = new File(confPath)
-    //val conf = ConfigFactory.parseFile(confFile).resolve()
-    val system = ActorSystem("graalvm-akka"/*, conf*/)
+    val confPath = if (args.length > 0) args(0) else "./reference.conf"
+    val confFile = new File(confPath)
+    val conf = ConfigFactory.parseFile(confFile).resolve()
+    val system = ActorSystem("graalvm-akka", conf)
     val pongMan = system.actorOf(Props[PongMan], "pong-man")
     val pingMan = system.actorOf(Props(classOf[PingMan], pongMan),"ping-man")
 
     implicit val ec = system.dispatcher
     implicit val timeout = Timeout(10.seconds)
-    val n = if (args.length == 1) args(0).toLong else 100000000
+    val n = 0L // Not Enabled
     val start = System.currentTimeMillis()
     val pong = Await.result(pingMan ? Ping(n), Duration.Inf).asInstanceOf[Pong]
     println(s"res = ${pong.s}, used ${System.currentTimeMillis() - start}ms")
